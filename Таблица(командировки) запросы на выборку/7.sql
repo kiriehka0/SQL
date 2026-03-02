51059bce8386023665784f7f333e4f7b5b22cd6a
kircatoon <hihka302@gmail.com>
2026-03-02 13:48:03 +0300

    Create 7.sql

---
+++ Таблица(командировки) запросы на выборку/7.sql
@@ -0,0 +1,11 @@
+SELECT name,
+        city,
+        date_first,
+        date_last
+FROM trip
+WHERE EXTRACT(MONTH FROM date_first) = EXTRACT(MONTH FROM date_last)
+ORDER BY city ASC, name ASC;
+
+
+
+
