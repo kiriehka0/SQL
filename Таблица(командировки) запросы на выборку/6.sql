cc0a1b0bddf416265520f8ac31d445eacd6b1d2b
kircatoon <hihka302@gmail.com>
2026-03-02 13:42:20 +0300

    Create 6.sql

---
+++ Таблица(командировки) запросы на выборку/6.sql
@@ -0,0 +1,13 @@
+SELECT name,
+        city,
+        date_first,
+        date_last
+FROM trip
+WHERE DATEDIFF(date_last, date_first) = (
+    SELECT MIN(DATEDIFF(date_last, date_first))
+    FROM trip);
+
+
+
+
+
