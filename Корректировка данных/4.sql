98d1e39aeb785f92a78c5fd2b46cb2aba145693a
kircatoon <hihka302@gmail.com>
2026-03-11 21:10:51 +0300

    Create 4.sql

---
+++ Корректировка данных/4.sql
@@ -0,0 +1,13 @@
+INSERT INTO book (title, author, price, amount)
+SELECT title,
+        author,
+        price,
+        amount
+FROM supply
+WHERE author NOT IN (
+            SELECT author
+            FROM book
+            );
+
+
+
